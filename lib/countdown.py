5fcbf7160f546bb2f5886dbc76bd743025a40f74
Seong Kim <runmath@gmail.com>
2023-07-16 20:30:43 -0600

    test done

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,14 @@
-# your code goes here!
+import time
+
+def countdown(num):
+    while num > 0:
+        print(f'{num} SECOND(S)!')
+        num -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(num):
+    while num > 0:
+        print(f'{num} SECOND(S)!')
+        num -= 1
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
